8af2f234d0ba399348db59cd7c30ecd26385f107
sumsung101 <yaraoley2005@mail.ru>
2022-02-02 22:45:30 +0300

    Initializing repository with Vue starter pack

---
+++ form-conformer/src/components/NavigationBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    components: { },
+})
+export default class NavigationBar extends Vue {}
+</script>
